ec2b23aa34ea31efa7e7b70cdeb92c4abf309f34
chewxy <chewxy@gmail.com>
2017-10-05 22:50:56 +1100

    ugh. accidental deletion of a key

--- dense_views.go
+++ dense_views.go
@@ -12,6 +12,6 @@ func (t *Dense) Materialize() Tensor {
 	retVal := recycledDense(t.t, t.shape.Clone())
 	copyDenseIter(retVal, t, nil, nil)
 	retVal.e = t.e
-	retVal.oe = t.o
+	retVal.oe = t.oe
 	return retVal
 }
