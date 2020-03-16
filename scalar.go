1f1019220da956557c9d42ea428b602882e1e0ef
chewxy <chewxy@gmail.com>
2020-03-16 13:09:22 +1100

    Added Scalar type to prepare for v0.10.0

---
+++ scalar.go
@@ -0,0 +1,81 @@
+// +build ignore
+
+package tensor
+
+import (
+	"fmt"
+	"io"
+	"reflect"
+	"unsafe"
+
+	"github.com/pkg/errors"
+	"gorgonia.org/tensor/internal/storage"
+)
+
+var _ Tensor = Scalar{}
+
+// Scalar is a representation of a scalar value on the CPU.
+type Scalar struct{ v interface{} }
+
+func MakeScalar(v interface{}) Scalar {
+	if s, ok := v.(Scalar); ok {
+		return s
+	}
+	if s, ok := v.(*Scalar); ok {
+		return Scalar{s.v}
+	}
+	return Scalar{v}
+}
+
+func (s Scalar) Shape() Shape           { return ScalarShape() }
+func (s Scalar) Strides() []int         { return nil }
+func (s Scalar) Dtype() Dtype           { return Dtype{reflect.TypeOf(s.v)} }
+func (s Scalar) Dims() int              { return 0 }
+func (s Scalar) Size() int              { return 0 } // TODO
+func (s Scalar) DataSize() int          { return 0 }
+func (s Scalar) RequiresIterator() bool { return false }
+func (s Scalar) Iterator() Iterator     { return nil }
+func (s Scalar) DataOrder() DataOrder   { return 0 } // TODO
+
+func (s Scalar) Slice(...Slice) (View, error)        { return nil, errors.New("Cannot slice a scalar") }
+func (s Scalar) At(at ...int) (interface{}, error)   { return nil, errors.New("Get a value of a scalar") }
+func (s Scalar) SetAt(_ interface{}, _ ...int) error { return errors.New("Cannot set value of scalar") }
+func (s Scalar) Reshape(_ ...int) error              { return errors.New("Cannot reshape a scalar") }
+func (s Scalar) T(_ ...int) error                    { return errors.New("Cannot transpose a scalar") }
+func (s Scalar) UT()                                 {}
+func (s Scalar) Transpose() error                    { return errors.New("Cannot transpose a scalar") }
+func (s Scalar) Apply(fn interface{}, opts ...FuncOpt) (Tensor, error) {
+	// TODO
+	return nil, errors.New("Cannot apply ")
+}
+
+func (s Scalar) Zero()                     {} //TODO
+func (s Scalar) Memset(interface{}) error  { return errors.New("Cannot Memset") }
+func (s Scalar) Data() interface{}         { return s.v }
+func (s Scalar) Eq(other interface{}) bool { return s == other }
+func (s Scalar) Clone() interface{}        { return s }
+
+func (s Scalar) IsScalar() bool           { return true }
+func (s Scalar) ScalarValue() interface{} { return s.v }
+
+func (s Scalar) Engine() Engine             { return nil }
+func (s Scalar) MemSize() uintptr           { return 0 }
+func (s Scalar) Uintptr() uintptr           { return 0 }
+func (s Scalar) Pointer() unsafe.Pointer    { return nil }
+func (s Scalar) IsNativelyAccessible() bool { return true }
+func (s Scalar) IsManuallyManaged() bool    { return false }
+
+func (s Scalar) Format(t fmt.State, c rune) {} // TODO
+func (s Scalar) String() string             { return fmt.Sprintf("%v", s) }
+
+func (s Scalar) WriteNpy(io.Writer) error { return errors.Errorf(methodNYI, "WriteNpy", "Scalar") }
+func (s Scalar) ReadNpy(io.Reader) error  { return errors.Errorf(methodNYI, "ReadNypy", "Scalar") }
+func (s Scalar) GobEncode() ([]byte, error) {
+	return nil, errors.Errorf(methodNYI, "GobEncode", "Scalar")
+}
+func (s Scalar) GobDecode([]byte) error { return errors.Errorf(methodNYI, "GobDecode", "Scalar") }
+
+func (s Scalar) standardEngine() standardEngine { return StdEng{} }
+func (s Scalar) hdr() *storage.Header           { return nil }
+func (s Scalar) arr() array                     { return array{} }
+func (s Scalar) arrPtr() *array                 { return nil }
