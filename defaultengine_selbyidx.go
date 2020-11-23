27434fe7cd71fa2539deba7406458b6cd71ab6c9
chewxy <chewxy@gmail.com>
2020-11-23 12:47:04 +1100

    Added the backwards function for SelectByIndices

--- defaultengine_selbyidx.go
+++ defaultengine_selbyidx.go
@@ -59,7 +59,7 @@ func (e StdEng) SelectByIndices(a, b Tensor, axis int, opts ...FuncOpt) (retVal 
 		return
 	}
 
-	e.selectByIdx(axis, typ, dataA, dataB, dataReuse, a.(*Dense).AP, b.(*Dense).AP, reuse.(*Dense).AP)
+	e.selectByIdx(axis, dataB.Ints(), typ, dataA, dataReuse, a.(*Dense).AP, reuse.(*Dense).AP)
 	return reuse, nil
 }
 
@@ -67,10 +67,9 @@ func (e StdEng) iterSelectByIdx(axis int, dataA, dataB, dataReuse *storage.Heade
 	panic("iterSelectByIdx is not yet implemented")
 }
 
-func (e StdEng) selectByIdx(axis int, typ reflect.Type, dataA, dataB, dataRetVal *storage.Header, apA, apB, apRet AP) {
+func (e StdEng) selectByIdx(axis int, indices []int, typ reflect.Type, dataA, dataRetVal *storage.Header, apA, apRet AP) {
 	isInnermost := axis == apA.shape.Dims()-1
 
-	indices := dataB.Ints()
 	outer := ProdInts(apA.shape[:axis])
 
 	axStride := apA.strides[axis]
@@ -125,5 +124,114 @@ func (e StdEng) selectByIdx(axis int, typ reflect.Type, dataA, dataB, dataRetVal
 			dstStart = dstEnd + (outerRetStride - retStride)
 		}
 	}
+}
 
+// SelectByIndicesB is the backwards function of SelectByIndices.
+func (e StdEng) SelectByIndicesB(a, b, indices Tensor, axis int, opts ...FuncOpt) (retVal Tensor, err error) {
+	if !indices.Shape().IsVectorLike() {
+		return nil, errors.Errorf("Expected indices to be a vector. Got %v instead", b.Shape())
+	}
+	if indices.Dtype() != Int {
+		return nil, errors.Errorf("Expected indices to be a vector of ints. Got %v instead", b.Dtype())
+	}
+
+	// if b is a scalar, then use Slice
+	if a.Shape().IsScalarEquiv() {
+		slices := make([]Slice, a.Shape().Dims())
+		slices[axis] = ss(b.Data().([]int)[0])
+		return a.Slice(slices...)
+	}
+
+	expectedShape := a.Shape().Clone()
+
+	var reuse DenseTensor
+	var _, toReuse, _ bool
+	if reuse, _, toReuse, _, _, err = handleFuncOpts(a.Shape(), a.Dtype(), a.DataOrder(), true, opts...); err != nil {
+		return nil, errors.Wrap(err, "Unable to handle funcOpts")
+	}
+	if !toReuse && reuse == nil {
+		// create reuse
+		reuse = New(WithShape(expectedShape...), Of(a.Dtype()))
+	}
+
+	typ := a.Dtype().Type
+	var _, dataB, dataReuse *storage.Header
+	var _, bit, iit Iterator
+	var useIter bool
+	if _, dataB, dataReuse, _, bit, iit, useIter, _, err = prepDataVV(a, b, reuse); err != nil {
+		return nil, errors.Wrapf(err, "StdEng.SelectByIndicesB")
+	}
+
+	if useIter {
+		e.iterSelectByIndicesB(axis, dataB, dataReuse, bit, iit)
+		//TODO
+		return
+	}
+
+	e.selectByIndicesB(axis, indices.Data().([]int), typ, dataB, dataReuse, b.(*Dense).AP, reuse.(*Dense).AP)
+
+	return reuse, nil
+}
+
+func (e StdEng) iterSelectByIndicesB(axis int, dataB, dataGradA *storage.Header, bit, iit Iterator) {
+	panic("iterSelectByIndicesB not implemented yet")
+}
+
+func (e StdEng) selectByIndicesB(axis int, indices []int, typ reflect.Type, dataB, dataGradA *storage.Header, apB, apRet AP) {
+	isInnermost := axis == apB.shape.Dims()-1
+
+	outer := ProdInts(apB.shape[:axis])
+
+	axStride := apB.strides[axis]
+	retStride := apRet.strides[axis]
+	var outerRetStride int
+	if outer == 0 {
+		// then it's the outermost
+		outer = 1
+		outerRetStride = apRet.strides[axis] * 2
+	} else {
+		outerRetStride = apRet.strides[axis-1]
+	}
+
+	dstCoord := make([]int, apB.shape.Dims())
+	srcCoord := make([]int, apRet.shape.Dims())
+
+	if isInnermost {
+		retPrevStride := apB.strides[axis-1]
+		prevStride := apRet.strides[axis-1]
+		for i, idx := range indices {
+			dstCoord[axis] = idx
+			srcCoord[axis] = i
+			dstStart, _ := Ltoi(apB.shape, apB.strides, dstCoord...)
+			start, _ := Ltoi(apRet.shape, apRet.strides, srcCoord...)
+			for o := 0; o < outer; o++ {
+				dstEnd := dstStart + axStride
+				end := start + retStride
+
+				e.E.AddSliced(typ, dataGradA, dstStart, dstEnd, dataB, start, end)
+
+				dstStart += prevStride
+				start += retPrevStride
+
+			}
+		}
+		return
+	}
+
+	for i, idx := range indices {
+		dstCoord[axis] = idx
+		srcCoord[axis] = i
+		dstStart, _ := Ltoi(apB.shape, apB.strides, dstCoord...)
+		start, _ := Ltoi(apRet.shape, apRet.strides, srcCoord...)
+
+		for o := 0; o < outer; o++ {
+			dstEnd := dstStart + axStride
+			end := start + retStride
+
+			e.E.AddSliced(typ, dataGradA, dstStart, dstEnd, dataB, start, end)
+
+			dstStart = dstEnd + axStride
+			start = end + (outerRetStride - retStride)
+		}
+	}
 }
--- dense_selbyidx_test.go
+++ dense_selbyidx_test.go
@@ -80,3 +80,47 @@ func TestDense_SelectByIndices(t *testing.T) {
 	correct2 = []float64{1, 1, 1, 5, 5, 5, 9, 9, 9, 13, 13, 13, 17, 17, 17, 21, 21, 21}
 	assert.Equal(correct2, a2.Data())
 }
+
+func TestDense_SelectByIndicesB(t *testing.T) {
+
+	a := New(WithBacking([]float64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23}), WithShape(3, 2, 4))
+	indices := New(WithBacking([]int{1, 1}))
+
+	t.Logf("a\n%v", a)
+
+	e := StdEng{}
+
+	a1, err := e.SelectByIndices(a, indices, 1)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	t.Logf("a1\n%v", a1)
+
+	a1Grad, err := e.SelectByIndicesB(a, a1, indices, 1)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	t.Logf("a1Grad \n%v", a1Grad)
+
+	a0, err := e.SelectByIndices(a, indices, 0)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	t.Logf("a0\n%v", a0)
+	a0Grad, err := e.SelectByIndicesB(a, a0, indices, 0)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	t.Logf("a0Grad\n%v", a0Grad)
+
+	a2, err := e.SelectByIndices(a, indices, 2)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	t.Logf("\n%v", a2)
+	a2Grad, err := e.SelectByIndicesB(a, a2, indices, 2)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	t.Logf("a2Grad\n%v", a2Grad)
+}
