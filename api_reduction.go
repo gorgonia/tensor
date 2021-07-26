fc40f1f45a8241344b462bf6355f4036cd799ed5
chewxy <chewxy@gmail.com>
2021-07-26 13:54:21 +1000

    Added max example

--- api_reduction.go
+++ api_reduction.go
@@ -2,12 +2,20 @@ package tensor
 
 import "github.com/pkg/errors"
 
-// Sum sums a Tensor along the given axes
+// Sum sums a Tensor along the given axes.
 func Sum(t Tensor, along ...int) (retVal Tensor, err error) {
 	if sumer, ok := t.Engine().(Sumer); ok {
 		return sumer.Sum(t, along...)
 	}
 	return nil, errors.New("Engine does not support Sum()")
+}
+
+// Max finds the maximum value along the given axes.
+func Max(t Tensor, along ...int) (retVal Tensor, err error) {
+	if maxer, ok := t.Engine().(Maxer); ok {
+		return maxer.Max(t, along...)
+	}
+	return nil, errors.New("Engine does not support Max()")
 }
 
 // Argmax finds the index of the max value along the axis provided
