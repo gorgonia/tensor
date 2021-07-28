1c8a1149e9cb6b510655cb8b14bdff327e005b83
chewxy <chewxy@gmail.com>
2021-07-28 22:00:09 +1000

    Unexported OpOpt.

--- funcopts.go
+++ funcopts.go
@@ -7,11 +7,14 @@ import (
 )
 
 // FuncOpt are optionals for calling Tensor functions.
-type FuncOpt func(*OpOpt)
+// The `*opOpt` type is unexported, but it's methods are exported.
+// This is intentional as use of the `*opOpt` is very specialized.
+// See funcopts.go for more information.
+type FuncOpt func(*opOpt)
 
 // WithIncr passes in a Tensor to be incremented.
 func WithIncr(incr Tensor) FuncOpt {
-	f := func(opt *OpOpt) {
+	f := func(opt *opOpt) {
 		opt.incr = incr
 	}
 	return f
@@ -19,7 +22,7 @@ func WithIncr(incr Tensor) FuncOpt {
 
 // WithReuse passes in a Tensor to be reused.
 func WithReuse(reuse Tensor) FuncOpt {
-	f := func(opt *OpOpt) {
+	f := func(opt *opOpt) {
 		opt.reuse = reuse
 	}
 	return f
@@ -27,7 +30,7 @@ func WithReuse(reuse Tensor) FuncOpt {
 
 // UseSafe ensures that the operation is a safe operation (copies data, does not clobber). This is the default option for most methods and functions
 func UseSafe() FuncOpt {
-	f := func(opt *OpOpt) {
+	f := func(opt *opOpt) {
 		opt.unsafe = false
 	}
 	return f
@@ -35,7 +38,7 @@ func UseSafe() FuncOpt {
 
 // UseUnsafe ensures that the operation is an unsafe operation - data will be clobbered, and operations performed inplace
 func UseUnsafe() FuncOpt {
-	f := func(opt *OpOpt) {
+	f := func(opt *opOpt) {
 		opt.unsafe = true
 	}
 	return f
@@ -43,7 +46,7 @@ func UseUnsafe() FuncOpt {
 
 // AsSameType makes sure that the return Tensor is the same type as input Tensors.
 func AsSameType() FuncOpt {
-	f := func(opt *OpOpt) {
+	f := func(opt *opOpt) {
 		opt.same = true
 	}
 	return f
@@ -51,7 +54,7 @@ func AsSameType() FuncOpt {
 
 // As makes sure that the the return Tensor is of the type specified. Currently only works for FromMat64
 func As(t dtype.Dtype) FuncOpt {
-	f := func(opt *OpOpt) {
+	f := func(opt *opOpt) {
 		opt.t = t
 	}
 	return f
@@ -59,14 +62,14 @@ func As(t dtype.Dtype) FuncOpt {
 
 // WithContext allows a function to be called with a given context
 func WithContext(ctx context.Context) FuncOpt {
-	f := func(opt *OpOpt) {
+	f := func(opt *opOpt) {
 		opt.ctx = ctx
 	}
 	return f
 }
 
-// OpOpt are the options used to call ops
-type OpOpt struct {
+// opOpt are the options used to call ops
+type opOpt struct {
 	reuse  Tensor
 	incr   Tensor
 	unsafe bool
@@ -76,7 +79,7 @@ type OpOpt struct {
 }
 
 // ParseFuncOpts parses a list of FuncOpt into a single unified method call structure.
-func ParseFuncOpts(opts ...FuncOpt) *OpOpt {
+func ParseFuncOpts(opts ...FuncOpt) *opOpt {
 	retVal := borrowOpOpt()
 
 	for _, opt := range opts {
@@ -89,13 +92,13 @@ func ParseFuncOpts(opts ...FuncOpt) *OpOpt {
 }
 
 // Incr returns the tensor to be incremented in the call. Can be nil.
-func (fo *OpOpt) Incr() Tensor { return fo.incr }
+func (fo *opOpt) Incr() Tensor { return fo.incr }
 
 // Reuse returns the tensor to be reused in the call. Can be nil.
-func (fo *OpOpt) Reuse() Tensor { return fo.reuse }
+func (fo *opOpt) Reuse() Tensor { return fo.reuse }
 
-// IncReuse returns whether a reuse tensor is to be used as the incr Tensor
-func (fo *OpOpt) IncrReuse() (Tensor, bool) {
+// IncrReuse returns whether a reuse tensor is to be used as the incr Tensor
+func (fo *opOpt) IncrReuse() (Tensor, bool) {
 	if fo.incr != nil {
 		return fo.incr, true
 	}
@@ -103,10 +106,10 @@ func (fo *OpOpt) IncrReuse() (Tensor, bool) {
 }
 
 // Safe signals if the op is to be done safely
-func (fo *OpOpt) Safe() bool { return !fo.unsafe }
+func (fo *opOpt) Safe() bool { return !fo.unsafe }
 
 // Same signals if the op is to return the same type as its inputs
-func (fo *OpOpt) Same() bool { return fo.same }
+func (fo *opOpt) Same() bool { return fo.same }
 
 // As returns the dtype of the return value of the method call.
 // For example:
@@ -117,19 +120,19 @@ func (fo *OpOpt) Same() bool { return fo.same }
 //		a.Add(b, As(Int))
 // indicates that the result of `Add()` should be converted to a Tensor of Int.
 // Note that this function is not yet supported in most operations.
-func (fo *OpOpt) As() dtype.Dtype { return fo.t }
+func (fo *opOpt) As() dtype.Dtype { return fo.t }
 
 // Context returns a context.Context that may have been passed in as a function option.
-func (fo *OpOpt) Context() context.Context { return fo.ctx }
+func (fo *opOpt) Context() context.Context { return fo.ctx }
 
 // SetReuse allows the reuse parameter to be set.
-func (fo *OpOpt) SetReuse(reuse Tensor) { fo.reuse = reuse }
+func (fo *opOpt) SetReuse(reuse Tensor) { fo.reuse = reuse }
 
 // SetIncr allows the incr parameter to be set.
-func (fo *OpOpt) SetIncr(incr Tensor) { fo.incr = incr }
+func (fo *opOpt) SetIncr(incr Tensor) { fo.incr = incr }
 
 // FuncOpts is the inverse of ParseFuncOpts.
-func (fo *OpOpt) FuncOpts() []FuncOpt {
+func (fo *opOpt) FuncOpts() []FuncOpt {
 	retVal := make([]FuncOpt, 0, 4)
 	if fo.reuse != nil {
 		retVal = append(retVal, WithReuse(fo.reuse))
