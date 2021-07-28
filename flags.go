45f2901a07c3b37f33966c9bd87a3825b0a5853f
chewxy <chewxy@gmail.com>
2021-07-28 15:58:00 +1000

    Moved funcopts into its own file because it should be in its own file. Also, added a WithContext() FuncOpt.

--- flags.go
+++ flags.go
@@ -1,7 +1,5 @@
 package tensor
 
-import "gorgonia.org/dtype"
-
 // DataOrder is a flag that indicates the order of data. The default DataOrder (0)
 // is what this package uses by default.
 type DataOrder byte
@@ -118,79 +116,3 @@ func MakeMemoryFlag(fs ...MemoryFlag) (retVal MemoryFlag) {
 func (f MemoryFlag) nativelyAccessible() bool { return !((f & NativelyInaccessible) != 0) }
 func (f MemoryFlag) manuallyManaged() bool    { return (f & ManuallyManaged) != 0 }
 func (f MemoryFlag) isOverallocated() bool    { return (f & IsOverallocated) != 0 }
-
-// OpOpt are the options used to call ops
-type OpOpt struct {
-	reuse  Tensor
-	incr   Tensor
-	unsafe bool
-	same   bool
-	t      dtype.Dtype
-}
-
-// ParseFuncOpts parses a list of FuncOpt into a single unified method call structure.
-func ParseFuncOpts(opts ...FuncOpt) *OpOpt {
-	retVal := borrowOpOpt()
-	for _, opt := range opts {
-		opt(retVal)
-	}
-	return retVal
-}
-
-// Incr returns the tensor to be incremented in the call. Can be nil.
-func (fo *OpOpt) Incr() Tensor { return fo.incr }
-
-// Reuse returns the tensor to be reused in the call. Can be nil.
-func (fo *OpOpt) Reuse() Tensor { return fo.reuse }
-
-// IncReuse returns whether a reuse tensor is to be used as the incr Tensor
-func (fo *OpOpt) IncrReuse() (Tensor, bool) {
-	if fo.incr != nil {
-		return fo.incr, true
-	}
-	return fo.reuse, false
-}
-
-// Safe signals if the op is to be done safely
-func (fo *OpOpt) Safe() bool { return !fo.unsafe }
-
-// Same signals if the op is to return the same type as its inputs
-func (fo *OpOpt) Same() bool { return fo.same }
-
-// As returns the dtype of the return value of the method call.
-// For example:
-//		a.Lt(b, As(Bool))
-// indicates that the result of the `Lt()` should be a Tensor of Bool.
-//
-// Another example:
-//		a.Add(b, As(Int))
-// indicates that the result of `Add()` should be converted to a Tensor of Int.
-// Note that this function is not yet supported in most operations.
-func (fo *OpOpt) As() dtype.Dtype { return fo.t }
-
-// SetReuse allows the reuse parameter to be set.
-func (fo *OpOpt) SetReuse(reuse Tensor) { fo.reuse = reuse }
-
-// SetIncr allows the incr parameter to be set.
-func (fo *OpOpt) SetIncr(incr Tensor) { fo.incr = incr }
-
-// FuncOpts is the inverse of ParseFuncOpts.
-func (fo *OpOpt) FuncOpts() []FuncOpt {
-	retVal := make([]FuncOpt, 0, 4)
-	if fo.reuse != nil {
-		retVal = append(retVal, WithReuse(fo.reuse))
-	}
-	if fo.incr != nil {
-		retVal = append(retVal, WithIncr(fo.incr))
-	}
-	if fo.unsafe {
-		retVal = append(retVal, UseUnsafe())
-	}
-	if fo.same {
-		retVal = append(retVal, AsSameType())
-	}
-	if fo.t != (Dtype{}) {
-		retVal = append(retVal, As(fo.t))
-	}
-	return retVal
-}
