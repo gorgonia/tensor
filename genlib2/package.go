8bb1cc3ef7e9f579c5c65237e94cbf9830694926
chewxy <chewxy@gmail.com>
2021-07-29 11:54:33 +1000

    Moved native iterators into package `tensor` proper. It is unexported in the `tensor` package as leaving it exported would lead to pollution of API. For backwards compatibility, the `native` package is still used. golinkname stubs are used in that case

--- genlib2/package.go
+++ genlib2/package.go
@@ -8,17 +8,27 @@ import (
 func writePkgName(f io.Writer, pkg string) {
 	switch pkg {
 	case tensorPkgLoc:
-		fmt.Fprintf(f, "// %s\n\npackage tensor\n\n", genmsg)
+		fmt.Fprintf(f, "package tensor\n\n // %s\n\n", genmsg)
 	case nativePkgLoc:
-		fmt.Fprintf(f, "// %s\n\npackage native\n\n", genmsg)
+		fmt.Fprintf(f, "package native\n\n // %s\n\n", genmsg)
+	case nativePkgLoc + "_unsafe":
+		fmt.Fprintf(f, "// +build !purego \n\npackage native\n\n // %s\n\n", genmsg)
+	case nativePkgLoc + "_purego":
+		fmt.Fprintf(f, "// +build purego \n\npackage native\n\n // %s\n\n", genmsg)
 	case execLoc:
-		fmt.Fprintf(f, "// %s\n\npackage execution\n\n", genmsg)
+		fmt.Fprintf(f, "package execution\n\n // %s\n\n", genmsg)
 	case storageLoc:
-		fmt.Fprintf(f, "// %s\n\npackage storage\n\n", genmsg)
+		fmt.Fprintf(f, "package storage\n\n // %s\n\n", genmsg)
 	default:
-		fmt.Fprintf(f, "// %s\n\npackage unknown\n\n", genmsg)
+		fmt.Fprintf(f, "package unknown\n\n %s\n\n", genmsg)
 	}
 }
 
 const importUnqualifiedTensor = `import . "gorgonia.org/tensor"
+`
+
+const importInternalNative = `import inative "gorgonia.org/tensor/internal/native"
+`
+
+const importUnsafe = `import _ "unsafe"
 `
