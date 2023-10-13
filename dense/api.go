bb20e94c15e422cac02d6cceefd7e9b9d41dba4b
chewxy <chewxy@gmail.com>
2023-10-13 20:12:40 +1100

    Added API for Norm. Now to test

--- dense/api.go
+++ dense/api.go
@@ -6,6 +6,7 @@ import (
 	"gorgonia.org/tensor"
 	"gorgonia.org/tensor/internal/errors"
 	"gorgonia.org/tensor/internal/execution"
+	"gorgonia.org/tensor/internal/specialized"
 )
 
 // Max returns the maximum value of the elements in the tensor. To return the maximum value of along one or more particular axes, use the Along funcopt.
@@ -68,6 +69,43 @@ func Prod[DT Num](a *Dense[DT], opts ...FuncOpt) (retVal *Dense[DT], err error) 
 
 	var z DT = 1
 	return a.Reduce(module, z, opts...)
+}
+
+// Norm returns the norm of the tensor. To return the norm of along one or more particular axes, use the Along funcopt.
+//
+// Norm is a generalization of the Pythagorean theorem. It is a function that assigns a strictly positive length or size to each vector in a vector space, other than the zero vector, and that satisfies the conditions:
+//
+// 1. Norm of a scalar is the absolute value of the scalar
+// 2. Triangle inequality: Norm(a + b) <= Norm(a) + Norm(b)
+// 3. Homogeneity: Norm(alpha * a) = |alpha| * Norm(a)
+func Norm[DT constraints.Float](a *Dense[DT], order tensor.NormOrder, opts ...FuncOpt) (retVal *Dense[DT], err error) {
+	e := getEngine[DT](a)
+	if err = check(checkFlags(e, a)); err != nil {
+		return nil, errors.Wrapf(err, errors.FailedSanity, errors.ThisFn())
+	}
+
+	var prepper specialized.FuncOptHandler[DT, *Dense[DT]]
+	var ok bool
+	if prepper, ok = e.(specialized.FuncOptHandler[DT, *Dense[DT]]); !ok {
+		return nil, errors.Errorf(errors.EngineSupport, e, prepper, errors.ThisFn(1))
+	}
+	var fo Option
+	if _, fo, err = prepper.HandleFuncOptsSpecialized(a, a.Shape(), opts...); err != nil {
+		return nil, errors.Wrapf(err, errors.FailedFuncOpt, errors.ThisFn())
+	}
+	ctx := fo.Ctx
+	along := fo.Along
+	if fo.Incr {
+		return nil, errors.Errorf(errors.NYIPR, "Norm", "WithIncr")
+	}
+
+	var normer tensor.Normer[DT, *Dense[DT]]
+	if normer, ok = e.(tensor.Normer[DT, *Dense[DT]]); !ok {
+		return nil, errors.Errorf(errors.EngineSupport, e, normer, errors.ThisFn())
+	}
+	ret := normer.Norm(ctx, a, order, along)
+	return ret.(*Dense[DT]), nil
+
 }
 
 func Lt[DT OrderedNum](a, b *Dense[DT], opts ...FuncOpt) (retVal DescWithStorage, err error) {
