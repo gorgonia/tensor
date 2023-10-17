29568c8cce6ee52a1f7512b4591a4145541e30e2
chewxy <xuanyi.chew@nine.com.au>
2023-10-18 10:51:06 +1100

    make scalar implement tensor.Basic (might not be a good idea). TO REVISIT

--- scalar/alias.go
+++ scalar/alias.go
@@ -7,3 +7,13 @@ import (
 
 type AP = tensor.AP
 type Engine = internal.Engine
+
+type DescWithStorage = tensor.DescWithStorage
+type ConsOpt = tensor.ConsOpt
+type DataOrder = internal.DataOrder
+
+type MemoryFlag = internal.MemoryFlag
+
+const (
+	ColMajor = internal.ColMajor
+)
