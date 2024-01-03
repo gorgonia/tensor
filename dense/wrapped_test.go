d9476f4113aa8c187d4a18091baa938fa9aecfed
chewxy <chewxy@gmail.com>
2024-01-04 10:20:10 +1100

    Fixed tests so they can run

--- dense/wrapped_test.go
+++ dense/wrapped_test.go
@@ -32,6 +32,10 @@ func (t *Wrapped[DT]) Scan(fn func(a, b DT) DT, axis int, opts ...FuncOpt) (*Wra
 	panic("NYI")
 }
 
+func (t *Wrapped[DT]) Dot(reductionFn, elwiseFn func(DT, DT) DT, other *Wrapped[DT], opts ...FuncOpt) (*Wrapped[DT], error) {
+	panic("NYI")
+}
+
 func (t *Wrapped[DT]) Slice(rs ...SliceRange) (*Wrapped[DT], error) { panic("NYI") }
 
 func (t *Wrapped[DT]) T(axes ...int) (*Wrapped[DT], error) { panic("NYI") }
@@ -43,6 +47,8 @@ func (t *Wrapped[DT]) Materialize() (*Wrapped[DT], error) { panic("NYI") }
 func (t *Wrapped[DT]) Alike(opts ...ConsOpt) *Wrapped[DT] {
 	return &Wrapped[DT]{t.Dense.Alike(opts...)}
 }
+
+func (t *Wrapped[DT]) Repeat(axis int, repeats ...int) (*Wrapped[DT], error) { panic("NYI") }
 
 func (t *Wrapped[DT]) AlikeAsType(dt dtype.Dtype, opts ...ConsOpt) DescWithStorage {
 	panic("NYI")
