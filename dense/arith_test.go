4ccb39f2fb22ebefd1013efe191ff4842cd5e726
chewxy <xuanyi.chew@nine.com.au>
2024-01-09 14:20:58 +1100

    Fixed quickcheck testing functions. Now we can quickcheck

--- dense/arith_test.go
+++ dense/arith_test.go
@@ -6,37 +6,100 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"gorgonia.org/tensor"
+	"gorgonia.org/tensor/internal"
 	gutils "gorgonia.org/tensor/internal/utils"
 )
 
-func TestDense_Add(t *testing.T) {
-	t.SkipNow()
-	iden := func(a *Dense[float64]) bool {
-		b := New[float64](WithShape(a.Shape().Clone()...), WithEngine(a.Engine()))
+func genAddIden[DT internal.OrderedNum](t *testing.T, assert *assert.Assertions) any {
+	return func(a *Dense[DT]) bool {
+		b := New[DT](WithShape(a.Shape().Clone()...), WithEngine(a.Engine()))
 		correct := a.Clone()
+
 		var we bool
 		if !a.IsNativelyAccessible() {
 			we = true
 		}
-		_, ok := a.Engine().(tensor.Adder[float64, *Dense[float64]])
+		_, ok := a.Engine().(tensor.Adder[DT, *Dense[DT]])
 		we = we || !ok
 
 		ret, err := a.Add(b)
 		if err, retEarly := qcErrCheck(t, "Add", a, b, we, err); retEarly {
-			if err != nil {
-				return false
-			}
-			return true
+			return err == nil
 		}
+		return assert.True(correct.Shape().Eq(ret.Shape()), "Expected %v. Got %v", correct.Shape(), ret.Shape()) &&
+			assert.Equal(correct.Data(), ret.Data())
 
-		if !qcEqCheck(t, a.Dtype(), false, correct.Data(), ret.Data()) {
+	}
+}
+
+func genAddIdenUnsafe[DT internal.OrderedNum](t *testing.T, assert *assert.Assertions) any {
+	return func(a *Dense[DT]) bool {
+		b := New[DT](WithShape(a.Shape().Clone()...), WithEngine(a.Engine()))
+		correct := a.Clone()
+
+		var we bool
+		if !a.IsNativelyAccessible() {
+			we = true
+		}
+		_, ok := a.Engine().(tensor.Adder[DT, *Dense[DT]])
+		we = we || !ok
+
+		ret, err := a.Add(b, tensor.UseUnsafe)
+		if err, retEarly := qcErrCheck(t, "Add (Unsafe)", a, b, we, err); retEarly {
+			return err == nil
+		}
+		return assert.Same(ret, a) &&
+			assert.True(correct.Shape().Eq(ret.Shape()), "Expected %v. Got %v", correct.Shape(), ret.Shape()) &&
+			assert.Equal(correct.Data(), ret.Data())
+
+	}
+}
+
+func genAddIdenReuse[DT internal.OrderedNum](t *testing.T, assert *assert.Assertions) any {
+	return func(a *Dense[DT]) bool {
+		b := New[DT](WithShape(a.Shape().Clone()...), WithEngine(a.Engine()))
+		correct := a.Clone()
+		reuse := a.Clone()
+		if err := reuse.Memset(1); err != nil {
 			return false
 		}
-		return true
+
+		var we bool
+		if !a.IsNativelyAccessible() {
+			we = true
+		}
+		_, ok := a.Engine().(tensor.Adder[DT, *Dense[DT]])
+		we = we || !ok
+
+		ret, err := a.Add(b, tensor.WithReuse(reuse))
+		if err, retEarly := qcErrCheck(t, "Add (Reuse)", a, b, we, err); retEarly {
+			return err == nil
+		}
+		return assert.Same(ret, reuse) &&
+			assert.True(correct.Shape().Eq(ret.Shape()), "Expected %v. Got %v", correct.Shape(), ret.Shape()) &&
+			assert.Equal(correct.Data(), ret.Data())
+
 	}
-	if err := quick.Check(iden, &quick.Config{Rand: newRand(), MaxCount: quickchecks}); err != nil {
+}
+
+func qcHelper[DT internal.OrderedNum](t *testing.T, assert *assert.Assertions, gen func(*testing.T, *assert.Assertions) any) {
+	t.Helper()
+	conf := &quick.Config{
+		Rand:     newRand(),
+		MaxCount: quickchecks,
+		Values:   qcDense[DT],
+	}
+
+	if err := quick.Check(gen(t, assert), conf); err != nil {
 		t.Errorf("Identity test for Add failed: %v", err)
 	}
+}
+
+func TestDense_Add(t *testing.T) {
+	assert := assert.New(t)
+	qcHelper[float64](t, assert, genAddIden[float64])
+	qcHelper[float64](t, assert, genAddIdenUnsafe[float64])
+	qcHelper[float64](t, assert, genAddIdenReuse[float64])
 }
 
 func TestDense_Add_manual(t *testing.T) {
