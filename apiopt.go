fa1f1ec0d535720d430fe8f8fd333d4be2db7d37
chewxy <xuanyi.chew@nine.com.au>
2024-03-19 15:20:29 +1100

    fixed broadcast bug

--- apiopt.go
+++ apiopt.go
@@ -1,4 +1,22 @@
 package tensor
 
+import (
+	"gorgonia.org/tensor/internal"
+	"gorgonia.org/tensor/internal/errors"
+)
+
 type APIOptions struct{}
 type APIOpt func(*APIOptions)
+
+func postOpBroadcastReshape(behav internal.BroadcastBehaviour, t, u, retVal DescWithStorage) (err error) {
+	broadcastDir := behav.BroadcastShape()
+	switch broadcastDir {
+	case internal.BroadcastShapeLeft:
+		err = retVal.Reshape(u.Shape()...)
+	case internal.BroadcastShapeRight:
+		err = retVal.Reshape(t.Shape()...)
+	case internal.BroadcastShapeLeft | internal.BroadcastShapeRight:
+		err = errors.Errorf(errors.NYIPR, "reshaping for bidirectional broadcasting", "postOpBroadcast")
+	}
+	return
+}
